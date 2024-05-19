fad4bc6edcb3ac56e6416ead638aa2e1874332a5
Qwey <luisfilipems@outlook.com>
2024-05-19 19:21:56 -0300

    Tela principal da aplicação

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
+########################################
+########## TELAS DE NAVEGAÇÃO ##########
+########################################
+
+# Tela principal
+def ui_inicio() -> None:
+    """Imprime a tela principal."""
+
+    error=''
+    while True:
+        os.system('cls')
+        print(
+            '┎──────────────────────────────────────────────────────────┐\n' +
+            '┃ Bem-vindo ao seu repositório de receitas!                │\n' +
+            '┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┥\n' +
+            '┃ (1) Listar suas receitas                                 │\n' +
+            '┃ (2) Favoritas                                            │\n' +
+            '┃ (3) Filtrar por ingrediente                              │\n' + # Funcionalidade extra
+            '┃ (4) Filtrar por país                                     │\n' +
+            '┃ (5) Sortear uma receita                                  │\n' +
+            '┃ (.) Sair                                                 │\n' +
+            '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙')
+    
+        if error!='':
+            print(error)
+        error=''
+
+        user_input=input()
+
+        match(user_input):
+            case '1':
+                ui_receitas()
+            case '2':
+                ui_favoritos()
+            case '3':
+                ui_ingredientes()
+            case '4':
+                ui_pais()
+            case '5':
+                if len(receitas)>1:
+                    ui_mostrar_receita(0, [random.choice(receitas)])
+                else:
+                    error='Não há receitas registradas!'
+            case '.':
+                return
+            case _:
+                error=f'Valor inválido! ({user_input})'
+                
 # Função principal do programa
 def main():
     global receitas # Apontando que receitas é global.
