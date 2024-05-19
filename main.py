b0adecb3fa3c8979c6709dae8084953f9eee4fe7
Qwey <luisfilipems@outlook.com>
2024-05-19 18:23:47 -0300

    Tela do menu de receitas adicionada

--- main.py
+++ main.py
@@ -46,6 +46,69 @@ def ui_inicio() -> None:
             case _:
                 error=f'Valor inválido! ({user_input})'
 
+# Tela do menu de receitas
+def ui_receitas() -> None:
+    """Imprime a tela do menu de receitas."""
+    mensagem=''
+    while True:
+        os.system('cls')
+        print(
+            '┎──────────────────────────────────────────────────────────┐\n' +
+            '┃ Suas Receitas                                            │\n' +
+            '┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━━━━━━━┥\n' +
+            '┃ (1) Visualizar                │ (4) Remover              │\n' +
+            '┃ (2) Adicionar                 │ (.) Retornar             │\n' +
+            '┃ (3) Modificar                 │                          │\n' +
+            '┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━┥\n' +
+            '┃ Nome                                                     │')
+        
+        if len(receitas) != 0:
+            for i in receitas:
+                index = receitas.index(i)
+                print(f'┃ {index} {i['nome']}'.ljust(59)+'│')
+
+        else:
+            print(
+                '┃                 Adicione alguma receita                  │\n' +
+                '┃                 para visualizá-la aqui!                  │')
+        
+        print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙')
+        
+        if mensagem!='':
+            print(mensagem)
+        mensagem=''
+
+        user_input=input()
+
+        match(user_input):
+            case '1':
+                view=input("Digite o ID da receita que deseja visualizar\n")
+                try:
+                    ui_mostrar_receita(view, receitas)
+                except:
+                    mensagem=f'Valor inválido! ({view})'
+            case '2':
+                adicionar_receita()
+                salvar()
+            case '3':
+                receita_id=input("Digite o ID da receita que deseja modificar!\n")
+                try:
+                    modificar_receita(receita_id)
+                    salvar()
+                except:
+                    mensagem=f'Valor inválido! ({receita_id})'
+            case '4':
+                delete=input("Digite o ID da receita que deseja remover\n")
+                try:
+                    del receitas[int(delete)]
+                    salvar()
+                    mensagem="Receita removida com sucesso. Pressione enter para continuar."
+                except:
+                    mensagem=f'Valor inválido! ({delete})'
+            case '.':
+                return
+            case _:
+                mensagem=f'Valor inválido! ({user_input})'
 
 # Função principal do programa
 def main():
